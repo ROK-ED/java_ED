db7d2d365da6ec53b26f45bcab529dd27ba6ada7
admin <admin@DESKTOP-5SDM39T>
2021-10-20 13:46:52 +0900

    211020 ch0502

--- chapter05/src/com/yedam/java/ch0502/referenceTypeExemple.java
+++ chapter05/src/com/yedam/java/ch0502/referenceTypeExemple.java
@@ -3,22 +3,90 @@ package com.yedam.java.ch0502;
 public class referenceTypeExemple {
 
 	public static void main(String[] args) {
-		//int[]scores = new int[3]; // |0|0|0| 3개의 공간만이 생김 int 지정안해서
-		int[] scores = { 83, 90, 87};
-		
+		// int[]scores = new int[3]; // |0|0|0| 3개의 공간만이 생김 int 지정안해서
+		int[] scores = { 83, 90, 87 };
+
 		System.out.println("scores[0] : " + scores[0]);
 		System.out.println("scores[1] : " + scores[1]);
 		System.out.println("scores[2] : " + scores[2]);
+
+		int sum = 0;
+		// for(int i=0; i<=3; i++) ~~오류코드 Index 3 out of bounds for length 3 인덱스보다 값이
+		// 초과한다
+		for (int i = 0; i < 3; i++) {
+			sum += scores[i];
+		}
+
+		System.out.println("총합 :" + sum);
+		double avg = (double) sum / 3;
+		System.out.println("평균 :" + avg);
+
+		int[] scores1;
 		
+		// 변수 선언후 추가 선언 방법
+		scores1 = new int[] { 83, 90, 87 };
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println("scores1[" + i + "]=" + scores[i]);
+		}
+		
+		int resulrt = add(new int[] { 83, 90, 87 });
+		//int resulrt = add({ 83, 90, 87 }); //new int[]로 추가되는 변수 타입 결정
+		
+		
+		System.out.println("============================");
+		int[] arr1 = new int[3];
+		for(int i=0; i<3; i++) {
+			System.out.println("arr1["+i+"]="+arr1[i]);
+		}
+		
+		arr1[0] = 10;
+		arr1[1] = 20;
+		arr1[2] = 30;
+		
+		for(int i=0; i<3; i++) {
+			System.out.println("arr1["+i+"]="+arr1[i]);
+		}
+		
+		System.out.println("============================");
+		String[] arr3 = new String[3]; //배열 생성
+		//String str = new String();  //스트링 생성
+		for(int i=0; i<3; i++) {
+			System.out.println("arr3["+i+"]="+arr3[i]);
+		}
+		
+		//String = "";
+		arr3[0] = "1월";
+		arr3[1] = "2월";
+		arr3[2] = "3월";
+		for(int i=0; i<3; i++) {
+			System.out.println("arr3["+i+"]="+arr3[i]);
+		}
+		
+		System.out.println("============================");
+		int[] scores3 = {83,90,87};
+		
+		int sum3 = 0;
+		for(int i=0; i<scores3.length; i++)   //배열이 가진 길이
+			sum3 += scores3[i];
+		
+		double avg3 = (double)sum3 / scores3.length;
+		System.out.println("총합 :"+ sum3);
+		System.out.println("평균 :"+ avg3);
+					
+		
+		
+		
+		
+
+	}
+
+	public static int add(int[] scores) {
 		int sum = 0;
 		for(int i=0; i<3; i++) {
 			sum += scores[i];
 		}
-		
-		System.out.println("총합 :"+sum);
-		double avg = (double) sum / 3;
-		System.out.println("평균 :"+avg);
-		
+		return sum;
 
 	}
 
---
+++ chapter05/src/com/yedam/java/ch0502_02/referenceTypeExample.java
@@ -0,0 +1,108 @@
+package com.yedam.java.ch0502_02;
+
+public class referenceTypeExample {
+
+	public static void main(String[] args) {
+		if(args.length !=2) {
+			System.out.println("값의 수가 부족합니다.");
+			System.exit(0);       //시스템 강제종료
+		}
+		
+		String strNum1 = args[0];
+		String strNum2 = args[1];
+		
+		int num1 = Integer.parseInt(strNum1);
+		int num2 = Integer.parseInt(strNum2);
+		
+		int result = num1 + num2;
+		System.out.println(num1 + "+" + num2 + "=" + result );
+		
+		
+		// 200p 4번 문제
+		int max = 0;
+		int[] array = { 1, 5, 3, 8, 2 };
+		
+		for(int i=0; i<array.length; i++) {
+			System.out.println(array[i]);
+		    if(array[i]>max) {
+		    	max = array[i];
+		    }
+		} System.out.println(max);
+		
+		//161p 3번
+		// while문과 Math.rendom() 함수를 이용해서
+		//2개의 주사위를 던졌을 때 나오는 숫자를 (숫자1, 숫자2) 형태로 출력하고,
+		//숫자의 합의 5가 아니면 계속 주사위를 던지고, 숫자의 합이 5이면 실행을 멈추는 코드를 작성
+		//또한 주사위를 몇번 던졌는지 횟수를 출력하세요
+		// 숫자의 합이 5가 되는 조합 (1,4), (4,1) , (2,3), (3,2)
+		
+		
+		int count = 0;
+		
+		
+		while(true) {
+			
+			int dice1 = (int)(Math.random()*6)+1;
+			int dice2 = (int)(Math.random()*6)+1;
+			count++;
+			
+			int sum = dice1 + dice2;
+			if(sum ==5) {
+				System.out.println("("+dice1+", "+dice2+")");
+				break;
+				
+			}
+			
+		}
+		System.out.println("주사위를 던진 횟수는 총"+count+"번 입니다.");
+		
+		//다차원 배열
+		int[][] scores = new int[2][3];
+		
+		int[] score1 = new int[3];
+		int[] score2 = new int[3];
+		// int[] ->타입
+		
+		//int[][] scoreArray = new int[][2];
+		//scoreArray[0] = score1;
+		
+		int[][] mathScores = new int[2][3];
+		System.out.println(mathScores);
+		
+		for(int x=0; x<mathScores.length; x++) {
+		    System.out.println(mathScores[x]);
+			int[] temp = mathScores[x];
+			for(int y=0; y<temp.length; y++) {
+				System.out.println(temp[y]);
+			}
+		}
+
+		for(int x=0; x<mathScores.length; x++) {
+		    System.out.println(mathScores[x]);
+			//int[] temp = mathScores[x];
+			for(int y=0; y<mathScores[x].length; y++) {
+				System.out.println(mathScores[x][y]);
+			}
+		}
+		
+		System.out.println();
+		
+		int[][] englichScores = new int[2][];
+		//englichScores[0] //null
+
+		englichScores[0] = new int[5];
+		englichScores[1] = new int[3];
+		
+		System.out.println(englichScores.length);
+		for(int x=0; x<englichScores.length; x++) {
+			System.out.println();
+			System.out.println(englichScores[x].length);
+			for(int y=0; y<englichScores[x].length; y++) {
+				System.out.println(englichScores[x][y]);
+				
+			}
+		}
+		
+	}
+
+}
