54c6451e7a7427e11df5d712d87da89c7df33236
admin <admin@DESKTOP-5SDM39T>
2021-10-26 12:02:57 +0900

    211026 02

--- Chapter06_Home/src/com/yedam/java/home/StudentTest.java
+++ Chapter06_Home/src/com/yedam/java/home/StudentTest.java
@@ -8,6 +8,20 @@ public class StudentTest {
 		// Kim학생은 3과목을 수강합니다. 국어 점수가 70점, 수학 점수가 85점, 영어점수가 100점입니다.
 		// Student클래스와 Subject클래스를 생성한 후 두 학생의 과목성적과 총점을 각각 출력해 봅시다.
 		
+		Student s1 = new Student(1001, "Lee");
+		s1.addSubject("국어", 100);
+		s1.addSubject("수학", 50);
+		
+		Student s2 = new Student(1002,"Kim");
+		s2.addSubject("국어", 70);
+		s2.addSubject("수학", 85);
+		s2.addSubject("영어", 100);
+		
+		s1.showStudentInfo();
+		System.out.println();
+		s2.showStudentInfo();
+		
+		
 	}
 
 }
