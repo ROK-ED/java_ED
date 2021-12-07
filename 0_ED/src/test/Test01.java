6a15d52d187e503b631b73e55e7a8a795e6d4050
'ed' <'klaizen91@gmail.com'>
2021-12-07 17:31:34 +0900

    211207

--- 0_ED/src/test/Test01.java
+++ 0_ED/src/test/Test01.java
@@ -2,22 +2,29 @@ package test;
 
 
 public class Test01 {
-	public static void main(String[] args) {
-		int min = 2147483647;
-		System.out.println(min);
-	
-	
-	Calculator cal = new Calculator();
-	
-	cal.powerOn();
-	System.out.println(cal.plus(5, 3));
-	
-	Car myCar = new Car("포르쉐");
-	Car yourCar = new Car("벤츠");
-	
-	myCar.run();
-	yourCar.run();
-	
+		 static int[] searchIdx(int[] a, int n, int key, int[] idx) {
+		      n = -1;
+		      for (int i = 0; i < a.length; i++) {
+		         if (a[i] == key) {
+		            idx[++n] = i;
+		         }
+		      }
+		      return idx;
+		   }
 
-}
+		   public static void main(String[] args) {
+//		      Scanner sc = new Scanner(System.in);
+//		      System.out.println("요소수");
+//		      int num = sc.nextInt();
+		      int a[] = { 1, 9, 2, 9, 4, 6, 7, 9 };
+		      int idx[] = null;
+		      int count = 0;
+		      int key = 9;
+
+		      searchIdx(a, count, key, idx);
+		      System.out.println("result" + count);
+		      for (int i = 0; i < idx.length; i++) {
+		         System.out.println(idx[i]);
+		      }
+		   }
 }
